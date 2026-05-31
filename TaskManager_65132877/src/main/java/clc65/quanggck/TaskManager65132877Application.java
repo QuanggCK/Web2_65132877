aa70005e36362a1704a19956df24496076c3e484
QuanggCK <zaphkiel1610@gmail.com>
2026-05-31 09:34:32 +0700

    first commit

---
+++ TaskManager_65132877/src/main/java/clc65/quanggck/TaskManager65132877Application.java
@@ -0,0 +1,13 @@
+package clc65.quanggck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManager65132877Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManager65132877Application.class, args);
+	}
+
+}
---
+++ TaskManager_65132877/src/test/java/clc65/quanggck/TaskManager65132877ApplicationTests.java
@@ -0,0 +1,13 @@
+package clc65.quanggck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManager65132877ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
